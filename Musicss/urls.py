9acc53973d1a2fe50d2c112d969ce918e0ca4c2c
AkshayM2001 <akshaymatale2001@gmail.com>
2023-12-30 00:19:01 +0530

    configure static

--- Musicss/urls.py
+++ Musicss/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns #new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,8 +27,9 @@ urlpatterns = [
 ]
 
 
+urlpatterns += staticfiles_urlpatterns()
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
